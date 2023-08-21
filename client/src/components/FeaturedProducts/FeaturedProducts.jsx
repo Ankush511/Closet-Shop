ed74615a55db1db031b0ec9be17b4b22c427e345
Ankush Kashyap <511ankush007@gmail.com>
2023-08-21 15:59:35 +0530

    Pages done

---
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Card from "../Card/Card";
+import "./FeaturedProducts.scss";
+import useFetch from "../../hooks/useFetch";
+
+const FeaturedProducts = ({ type }) => {
+  const { data, loading, error } = useFetch(
+    `/products?populate=*&[filters][type][$eq]=${type}`
+  );
+
+  return (
+    <div className="featuredProducts">
+      <div className="top">
+        <h1>{type} Products</h1>
+        <p>
+        Discover our handpicked selection of featured products at Closet Shop, where style meets substance. These standout pieces have been chosen to embody the latest trends, timeless elegance, and unbeatable quality. Explore our featured products and unlock a world of limitless style possibilities.
+        </p>
+      </div>
+      <div className="bottom">
+        {error
+          ? "Something went wrong!"
+          : loading
+          ? "loading"
+          : data?.map((item) => <Card item={item} key={item.id} />)}
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProducts;
--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import React from "react";
+import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className="footer">
+      <div className="top">
+        <div className="item">
+          <h1>Categories</h1>
+          <span>Women</span>
+          <span>Men</span>
+          <span>Shoes</span>
+          <span>Accessories</span>
+          <span>New Arrivals</span>
+        </div>
+        <div className="item">
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stores</span>
+          <span>Compare</span>
+          <span>Cookies</span>
+        </div>
+        <div className="item">
+          <h1>About</h1>
+          <span>
+          Discover the epitome of style and comfort at Closet Shop, your ultimate online destination for exquisite clothing. Dive into a world of fashion where each garment tells a unique story of elegance and craftsmanship. Elevate your wardrobe today with Closet Shop and redefine your fashion journey with us.
+          </span>
+        </div>
+        <div className="item">
+          <h1>Contact</h1>
+          <span>
+          We value your thoughts and are eager to assist. Feel free to reach out to us at Closet Shop for any inquiries, concerns, or style advice. Our dedicated customer support team is here to provide you with top-notch service, ensuring your shopping experience is as seamless as slipping into your favorite outfit.
+          </span>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="left">
+          <span className="logo">Closet-Shop</span>
+          <span className="copyright">
+            © Copyright 2023. All Rights Reserved
+          </span>
+        </div>
+        <div className="right">
+          <img src="/img/payment.png" alt="" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
