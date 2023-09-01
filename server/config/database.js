a6b919eda92847636f91c51a0d131a16a6da67f4
Ankush Kashyap <511ankush007@gmail.com>
2023-09-01 17:20:05 +0530

    done

--- server/config/database.js
+++ server/config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'dpg-cjooqbe1208c73ek6bl0-a.singapore-postgres.render.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'store_38ma'),
-      user: env('DATABASE_USERNAME', 'store_38ma_user'),
-      password: env('DATABASE_PASSWORD', 'XgHCrmgCZlyFJ0pGunrbrgC9Rqy66Zsf'),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
       ssl: env.bool('DATABASE_SSL', true),
     },
   },
